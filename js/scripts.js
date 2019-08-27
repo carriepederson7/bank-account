a21e86d57f33126a24bc3eb89b2432514fda28ab
Student <example@example.com>
2019-08-27 16:47:58 -0700

    will update js and third form

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,10 @@ Directory.prototype.assignId = function(){
 }
 
 
+
+
+
+
 //Front-end
 
 $(document).ready(function(){
@@ -36,25 +40,49 @@ $(document).ready(function(){
     var newCustomer = new Customer(customerName, initialDeposit);
     newDirectory.addCustomer(newCustomer);
     console.log(newDirectory);
-    //$('.initial-deposit').hide();
-    $('.result').append("<li>" + "Welcome," + customerName + " your Account Balance is: " + initialDeposit + "<br>" + "Your Id is: " + newDirectory.currentId + "</li>");
+    $('.initial-deposit').hide();
+    $('#new-customer').hide();
+    $('.result').append("<li> Welcome," + customerName + " your Account Balance is: " + initialDeposit + "<br>" + "Your Id is: " + newDirectory.currentId + "</li>");
     //console.log(newDirectory.customer.id);
   });
 
+
+
   $('#form2').submit(function(event){
     event.preventDefault();
-    alert("inform");
-    // function findContact(newDirectory){
       var customerId = parseInt($('#user-id').val());
+      //this works because we took an instance of customer and used it throughout this function
       newDirectory.customers.forEach(function(customer){
         if (customer.id === customerId){
-          alert("found id");
-        $('.result2').append(customer.name);
-        //$('.deposits-and-withdrawls').show();
-        }
-
+        $('.result2 h3').append(customer.name);
+        $('.result2').append(customer.deposit);
+        $('.deposits-and-withdrawls').show();
+      }
     });
+
   });
 
+  $('#deposit-withdrawl').submit(function(event){
+    event.preventDefault();
+
+    var deposit = $("#deposit").val();
+    var withdrawl = $("#withdrawl").val();
+    var currentCustomer= $('#customer-name').val();
+    newDirectory.customers.forEach(function(customer){
+        console.log($('#customer-name').val());
+
+      if (customer.name === currentCustomer){
+        parseInt(customer.deposit)= parseInt(customer.deposit)+ parseInt(deposit);
+        console.log(customer.deposit);
+        $('.new-balance').append(customer.deposit);
+      }
+    });
+
+
+    //
+    // }
+
+
+});
 
 });
