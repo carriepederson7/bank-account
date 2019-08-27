6aec67ffe5363cbc207d298790b8edbbfdc2c304
Student <example@example.com>
2019-08-27 14:36:37 -0700

    this commit adds inputs to the right places

--- js/scripts.js
+++ js/scripts.js
@@ -35,11 +35,26 @@ $(document).ready(function(){
     var withdrawlAmount= parseInt($('#withdrawl').val());
     var newCustomer = new Customer(customerName, initialDeposit);
     newDirectory.addCustomer(newCustomer);
-    var newId = newCustomer.id;
     console.log(newDirectory);
-    $('.initial-deposit').hide();
+    //$('.initial-deposit').hide();
     $('.result').append("<li>" + "Welcome," + customerName + " your Account Balance is: " + initialDeposit + "<br>" + "Your Id is: " + newDirectory.currentId + "</li>");
-
+    //console.log(newDirectory.customer.id);
   });
+
+  $('#form2').submit(function(event){
+    event.preventDefault();
+    alert("inform");
+    // function findContact(newDirectory){
+      var customerId = parseInt($('#user-id').val());
+      newDirectory.customers.forEach(function(customer){
+        if (customer.id === customerId){
+          alert("found id");
+        $('.result2').append(customer.name);
+        //$('.deposits-and-withdrawls').show();
+        }
+
+    });
+  });
+
 
 });
